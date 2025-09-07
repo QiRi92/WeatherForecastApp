eee2217c9fe5f66c9fcf8fef68c063b7d11e5285
QiRi92 <abdulbaqi9201@gmail.com>
2025-09-07 13:55:28 +0800

    Initial commit: Weather Forecast App

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WeatherForecastApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetWeather = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCity
+            // 
+            this.txtCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCity.Location = new System.Drawing.Point(140, 19);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(222, 35);
+            this.txtCity.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter City:";
+            // 
+            // btnGetWeather
+            // 
+            this.btnGetWeather.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnGetWeather.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetWeather.Location = new System.Drawing.Point(403, 20);
+            this.btnGetWeather.Name = "btnGetWeather";
+            this.btnGetWeather.Size = new System.Drawing.Size(155, 35);
+            this.btnGetWeather.TabIndex = 2;
+            this.btnGetWeather.Text = "Get Weather";
+            this.btnGetWeather.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnGetWeather.UseVisualStyleBackColor = false;
+            this.btnGetWeather.Click += new System.EventHandler(this.btnGetWeather_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(13, 88);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(700, 200);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Weather info will appear here";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 341);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnGetWeather);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetWeather;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtCity;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace WeatherForecastApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private string LoadApiKey()
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "api_key.txt");
+
+        if (File.Exists(filePath))
+        {
+            return File.ReadAllText(filePath).Trim();
+        }
+        else
+        {
+            throw new Exception("API key file not found. Please create api_key.txt in the project folder.");
+        }
+    }
+    
+        private async void btnGetWeather_Click(object sender, EventArgs e)
+        {
+            string city = txtCity.Text;
+
+            if (string.IsNullOrWhiteSpace(city))
+            {
+                lblResult.Text = "Please enter a city name.";
+                return;
+            }
+
+            try
+            {
+                string json = await GetWeatherDataAsync(city);
+                string result = ParseWeather(json);
+                lblResult.Text = result;
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Error: {ex.Message}";
+            }
+        }
+
+        public async Task<string> GetWeatherDataAsync(string city)
+        {
+            string apiKey = LoadApiKey(); // 👈 Load from file
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+
+        public string ParseWeather(string json)
+        {
+            JObject data = JObject.Parse(json);
+            string description = data["weather"][0]["description"].ToString();
+            string temp = data["main"]["temp"].ToString();
+            return $"Temperature: {temp}°C\nCondition: {description}";
+        }
+    }
+}
